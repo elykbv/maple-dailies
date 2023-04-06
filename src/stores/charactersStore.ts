ecadbcb813b0329166eeab4bdcd5a2b6f17860a3
elykbv <elii.yakubov@gmail.com>
2023-04-06 13:54:27 -0400

    updated store logic to use dictionaries

--- src/stores/charactersStore.ts
+++ src/stores/charactersStore.ts
@@ -1,14 +1,46 @@
 import { create } from 'zustand'
+import { ICharacterData } from '../types/ICharacterData'
+import { IDailyData } from '../types/IDailyData'
+
+interface IDailiesData {
+  charInfo: ICharacterData
+  dailies: IDailyData
+}
 
 interface CharactersState {
-  characters: string[]
-  addCharacter: (ign: string) => void
-  removeCharacter: (ign: string) => void
+  characters: { [ign: string]: IDailiesData }
+  addCharacter: (charInfo: ICharacterData, dailies: IDailyData) => void
+  //   updateDailyStatus: (ign: string, daily: string, status: boolean) => void
+  //   removeCharacter: (ign: string) => void
 }
 
 export const useCharactersStore = create<CharactersState>()((set) => ({
-  characters: [],
-  addCharacter: (ign) =>
-    set((state) => ({ characters: [...state.characters, ign] })),
-  removeCharacter: (ign) => set((state) => state)
+  characters: {},
+  addCharacter: (charInfo, dailies) => {
+    set((state) => ({
+      characters: {
+        ...state.characters,
+        [charInfo.CharacterName]: { charInfo: charInfo, dailies: dailies }
+      }
+      //   characters: [
+      //     ...state.characters,
+      //     { charInfo: charInfo, dailies: dailies }
+      //   ]
+    }))
+  }
+  //   updateDailyStatus: (ign, daily, status) => {
+  //     set((state) => ({
+  //       characters: {
+  //         ...state.characters,
+  //         [ign]: {
+  //           ...state.characters[ign],
+  //           dailies: {
+  //             ...state.characters[ign].dailies
+  //           }
+  //         }
+  //       }
+  //     }))
+  //   }
+
+  //   removeCharacter: (ign) => set((state) => state)
 }))
