b0b7c700e34fe7c380e645ab35ef4357345489f4
elykbv <elii.yakubov@gmail.com>
2023-04-06 13:54:54 -0400

    constants for dailies and categories

---
+++ src/constants/DailyCategories.const.ts
@@ -0,0 +1,14 @@
+export const dailyCategories = [
+  {
+    category: 'daily-boss',
+    title: 'Daily Bosses'
+  },
+  {
+    category: 'symbol',
+    title: 'Symbol Dailies'
+  },
+  {
+    category: 'misc',
+    title: 'Misc.'
+  }
+]
---
+++ src/constants/DailyList.const.ts
@@ -0,0 +1,50 @@
+export const dailyList = [
+  {
+    name: 'Gollux',
+    type: 'daily-boss'
+  },
+  {
+    name: 'Arkarium',
+    type: 'daily-boss'
+  },
+  {
+    name: 'Magnus',
+    type: 'daily-boss'
+  },
+  {
+    name: 'Root Abyss',
+    type: 'daily-boss'
+  },
+  {
+    name: 'Ranmaru',
+    type: 'daily-boss'
+  },
+  {
+    name: 'Papalatus',
+    type: 'daily-boss'
+  },
+  {
+    name: 'Commerci',
+    type: 'misc'
+  },
+  {
+    name: 'Phantom Forest',
+    type: 'misc'
+  },
+  {
+    name: 'Vanishing Journey',
+    type: 'symbol'
+  },
+  {
+    name: 'Erda Spectrum',
+    type: 'symbol'
+  },
+  {
+    name: 'Chu Chu Island',
+    type: 'symbol'
+  },
+  {
+    name: 'Hungry Muto',
+    type: 'symbol'
+  }
+]
