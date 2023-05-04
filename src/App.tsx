7d3275d4b9598922726b2477626b65ebd543bb10
elykbv <elii.yakubov@gmail.com>
2023-05-03 20:48:56 -0400

    Begin working on refactoring from zustand to localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,85 +1,77 @@
-import { useState } from 'react'
-import { useForm, Controller } from 'react-hook-form'
-import CharacterCard from './components/CharacterCard/CharacterCard'
-import { ActionIcon, Button, Tabs, TextInput } from '@mantine/core'
-import { useCharactersStore } from './stores/charactersStore'
-import { useDisclosure } from '@mantine/hooks'
-import EditDailiesModal from './components/Modals/EditDailiesModal'
-import DailiesList from './components/DailiesList/DailiesList'
-import Timer from './components/Timer/Timer'
+import { useState } from "react";
+import { useForm, Controller } from "react-hook-form";
+import Nav from "./components/Nav/Nav";
+import CharacterCard from "./components/CharacterCard/CharacterCard";
+import { ActionIcon, Button, Tabs, TextInput } from "@mantine/core";
+import { useCharactersStore } from "./stores/charactersStore";
+import { useDisclosure, useLocalStorage } from "@mantine/hooks";
+import EditDailiesModal from "./components/Modals/EditDailiesModal";
+import DailiesList from "./components/DailiesList/DailiesList";
+import Timer from "./components/Timer/Timer";
+import { IgnFormValues } from "./types/IgnFormValues";
 
 function App() {
-  const { characters } = useCharactersStore((state) => state)
-  const { control, getValues } = useForm()
-  const [opened, { open, close }] = useDisclosure(false)
-  const [activeCharacter, setActiveCharacter] = useState<string | null>(
-    characters[0]?.charInfo.CharacterName
-  )
+    const [dailies, setDailies] = useLocalStorage<any>({
+        key: "maple-dailies-list",
+        defaultValue: [],
+    });
+    const { characters } = useCharactersStore((state) => state);
+    const { control, getValues } = useForm<IgnFormValues>();
+    const [opened, { open, close }] = useDisclosure(false);
+    const [activeCharacter, setActiveCharacter] = useState<any>(
+        dailies[0]// TODO
+    );
 
-  return (
-    <div>
-        <div style={{ display: 'flex', justifyContent: 'end', gap: 0 }}>
-        <Controller
-          name="ign"
-          defaultValue=""
-          rules={{
-            minLength: 4,
-            maxLength: 12
-          }}
-          control={control}
-          render={({ field: { value, onChange } }) => (
-            <TextInput
-              value={value}
-              onChange={onChange}
-              placeholder="Enter your IGN"
-              onKeyPress={e => e.key === 'Enter' && open()}
-            />
-          )}
-        />
-        <ActionIcon color="blue" size="lg" variant="filled" onClick={open}>
-          +
-        </ActionIcon>
-      </div>
-      <Tabs
-        value={activeCharacter}
-        onTabChange={setActiveCharacter}
-        variant="outline"
-      >
-        <Tabs.List>
-          {Object.keys(characters).map((character) => {
-            return (
-              <Tabs.Tab value={characters[character].charInfo.CharacterName}>
-                <CharacterCard
-                  ign={characters[character].charInfo.CharacterName}
-                  key={characters[character].charInfo.CharacterName}
-                  active={
-                    activeCharacter ===
-                    characters[character].charInfo.CharacterName
-                  }
-                  onClick={() =>
-                    setActiveCharacter(
-                      characters[character].charInfo.CharacterName
-                    )
-                  }
-                />
-              </Tabs.Tab>
-            )
-          })}
-        </Tabs.List>
-		</Tabs>
-        <div style={{ display: 'flex'}}>
-            <div>
+    console.log(dailies);
 
-        <Timer reset='daily'/>
-        <DailiesList dailies={characters[activeCharacter!]?.dailies} />
-        <EditDailiesModal opened={opened} close={close} ign={getValues().ign}/> 
-        </div>
+    return (
         <div>
-            <Timer reset='weekly' />
+            <Nav control={control} open={open} />
+            <Tabs
+                value={activeCharacter}
+                onTabChange={setActiveCharacter}
+                variant="outline"
+            >
+                <Tabs.List>
+                    {dailies.map((daily) => {
+                        console.log(daily)
+                        return (
+                            <Tabs.Tab value={daily.ign}>
+                                <CharacterCard
+                                    ign={daily.ign}
+                                    key={daily.ign}
+                                    active={activeCharacter?.ign === daily.ign}
+                                    onClick={() =>
+                                        setActiveCharacter({
+                                            ign: daily.ign,
+                                        })
+                                    }
+                                />
+                            </Tabs.Tab>
+                        );
+                    })}
+                </Tabs.List>
+            </Tabs>
+            <div style={{ display: "flex" }}>
+                <div>
+                    <Timer reset="daily" />
+                    <DailiesList
+                        dailies={characters[activeCharacter!]?.dailies}
+                    />
+                    <EditDailiesModal
+                        opened={opened}
+                        close={close}
+                        ign={getValues().ign}
+                        localStorageDailies={dailies}
+                        setLocalStorageDailies={setDailies}
+                    />
+                </div>
+                <div>
+                    <Timer reset="weekly" />
+                </div>
+            </div>
         </div>
-        </div>
-    </div>
-    )
+    );
 }
 
-export default App
+export default App;
--- src/components/Modals/EditDailiesModal.tsx
+++ src/components/Modals/EditDailiesModal.tsx
@@ -1,75 +1,95 @@
 import {
-  Checkbox,
-  Modal,
-  Button,
-  Title,
-  Flex,
-  Center,
-  Image
-} from '@mantine/core'
-import { useForm, Controller } from 'react-hook-form'
-import { useGetCharacter } from '../../hooks/useGetCharacter'
-import { useCharactersStore } from '../../stores/charactersStore'
-import { IDailyData } from '../../types/IDailyData'
-import { dailyList } from '../../constants/DailyList.const'
-import { dailyCategories } from '../../constants/DailyCategories.const'
+    Checkbox,
+    Modal,
+    Button,
+    Title,
+    Flex,
+    Center,
+    Image,
+} from "@mantine/core";
+import { useForm, Controller } from "react-hook-form";
+import { useGetCharacter } from "../../hooks/useGetCharacter";
+import { useCharactersStore } from "../../stores/charactersStore";
+import { IDailyData } from "../../types/IDailyData";
+import { dailyList } from "../../constants/DailyList.const";
+import { dailyCategories } from "../../constants/DailyCategories.const";
 
 interface Props {
-  opened: boolean
-  close: () => void
-  ign: string
+    opened: boolean;
+    close: () => void;
+    ign: string;
+    localStorageDailies: any; // TODO
+    setLocalStorageDailies: any; // TODO
 }
 
-const EditDailiesModal: React.FC<Props> = ({ opened, close, ign }) => {
-  const { handleSubmit, control, getValues } = useForm()
-  const { data, loading } = useGetCharacter(ign)
-  const addCharacter = useCharactersStore((state) => state.addCharacter)
+const EditDailiesModal: React.FC<Props> = ({
+    opened,
+    close,
+    ign,
+    localStorageDailies,
+    setLocalStorageDailies,
+}) => {
+    const { handleSubmit, control, getValues } = useForm();
+    const { data, loading } = useGetCharacter(ign);
+    const addCharacter = useCharactersStore((state) => state.addCharacter);
 
-  const onSubmit = () => {
-    let dailies = {} as IDailyData
-    for (const [key, value] of Object.entries(getValues())) {
-      if (value) dailies[key] = false
-    }
-    addCharacter(data!, dailies)
-    console.log(getValues())
-  }
+    const onSubmit = handleSubmit(() => {
+        let dailies = {} as IDailyData;
+        for (const [key, value] of Object.entries(getValues())) {
+            if (value) dailies[key] = false;
+        }
+        addCharacter(data!, dailies);
+        setLocalStorageDailies([
+            ...localStorageDailies,
+            {
+                ign: data?.CharacterName,
+                img: data?.CharacterImgUrl,
+                level: data?.Level,
+                dailies: dailies,
+            },
+        ]);
+    });
 
-  return (
-    <Modal opened={opened} onClose={close}>
-      <Center>
-        <Image src={data?.CharacterImgUrl} width={150} mb={30} />
-      </Center>
-      <Flex wrap="wrap" gap="xl">
-        {dailyCategories.map((category) => {
-          return (
-            <div style={{ width: '40%' }}>
-              <Title order={4}>{category.title}</Title>
-              {dailyList
-                .filter((daily) => daily.type === category.category)
-                .map((daily, index) => {
-                  return (
-                    <Controller
-                      key={index}
-                      control={control}
-                      defaultValue={false}
-                      name={`${daily.name}-${daily.type}`}
-                      render={({ field: { value, onChange } }) => (
-                        <Checkbox
-                          label={daily.name}
-                          onChange={onChange}
-                          checked={value}
-                        />
-                      )}
-                    />
-                  )
+    return (
+        <Modal opened={opened} onClose={close}>
+            <Center>
+                <Image src={data?.CharacterImgUrl} width={150} mb={30} />
+            </Center>
+            <Flex wrap="wrap" gap="xl">
+                {dailyCategories.map((category) => {
+                    return (
+                        <div style={{ width: "40%" }}>
+                            <Title order={4}>{category.title}</Title>
+                            {dailyList
+                                .filter(
+                                    (daily) => daily.type === category.category
+                                )
+                                .map((daily, index) => {
+                                    return (
+                                        <Controller
+                                            key={index}
+                                            control={control}
+                                            defaultValue={false}
+                                            name={`${daily.name}-${daily.type}`}
+                                            render={({
+                                                field: { value, onChange },
+                                            }) => (
+                                                <Checkbox
+                                                    label={daily.name}
+                                                    onChange={onChange}
+                                                    checked={value}
+                                                />
+                                            )}
+                                        />
+                                    );
+                                })}
+                        </div>
+                    );
                 })}
-            </div>
-          )
-        })}
-      </Flex>
-      <Button onClick={onSubmit}>Submit</Button>
-    </Modal>
-  )
-}
+            </Flex>
+            <Button onClick={onSubmit}>Submit</Button>
+        </Modal>
+    );
+};
 
-export default EditDailiesModal
+export default EditDailiesModal;
